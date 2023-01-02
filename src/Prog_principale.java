9926f29eeb4f9f3c412725f83545f4f1e3ed36c5
Your Name <you@example.com>
2023-01-02 12:40:12 +0100

    class utilisateur

--- src/Prog_principale.java
+++ src/Prog_principale.java
@@ -1,6 +1,7 @@
 public class Prog_principale {
     public static void main(String[] args) {
         
+    
         
     }
 }
--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,3 +1,46 @@
 public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String login;
+    private String pwd;
+
     
+
+    //CONSTRUCTOR
+    public Utilisateur(String nom,String prenom)
+        {
+            this.nom=nom;
+            this.prenom=prenom;
+        }
+
+//setter&getter nom
+    public String getNom(){
+        return this.nom;
+    }
+
+ 
+    public void setNom (String nom){
+        this.nom=nom;
+    }
+
+//setter&getter prenom
+     public String getPrenom(){
+        return this.prenom;
+    }
+
+
+    public void setPrenom (String prenom){
+        this.prenom=prenom;
+    }
+
+//aff nom & prenom de l'utilisateur
+    public void aff_Util(){
+        System.out.print("name"+this.nom+",prenom"+this.prenom);
+    }
+
+    
+
+
+
+
 }

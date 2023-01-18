931d288cd6d0502242cbef7d73a9a5b47557d8d3
souhir walha <118829106+souhirwalha03@users.noreply.github.com>
2023-01-18 17:02:19 +0100

    Update Prog_principale.java

--- src/Prog_principale.java
+++ src/Prog_principale.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class Prog_principale {
     public static void main(String[] args) {
         Client client1 = new Client(123,"emma", "mmmm", "emma03", "1234");
@@ -16,6 +17,34 @@ public class Prog_principale {
         Users.add(client2);
         Users.add(client3);
     
+
+        Scanner sc =new Scanner(System.in);
+
+        //// creation d'un tableau de vehicules
+        System.out.println("Donner le nombre de vehicules de l'agence");
+        int nombre = sc.nextInt();
+        Vehicule[] tableauvehicules = new Vehicule[nombre];
+        
+        //// remplissage du tableau 
+        for ( int i=1; i<tableauvehicules.length+1;i++) {
+        System.out.println("Entrer l'id de la véhicule"+ " " + i);
+        int idVehicule = sc.nextInt();
+        System.out.println("Entrer le kilometrage");
+        int kilometrage = sc.nextInt();
+        System.out.println("Entrer le nombre de places");
+        int nbrPlaces = sc.nextInt();
+        System.out.println("Entrer prix de location");
+        float prixlocation = sc.nextFloat();
+        System.out.println("Entrer la marque");
+        String marque = sc.next();
+        System.out.println("Entrer l'immatriculation");
+        String immatriculation = sc.next();
+        System.out.println("Entrer la couleur");
+        String couleur = sc.next();
+        System.out.println("Entrer 1 si elle est dispo et 0 sinon");
+        int dispo = sc.nextInt();
+        tableauvehicules[i] = new Vehicule();
+        }
 		
         
     }

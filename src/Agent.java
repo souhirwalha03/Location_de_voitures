5223f72745eb1ab4763ab54cf349e3adc4a832b2
souhir walha <118829106+souhirwalha03@users.noreply.github.com>
2023-01-18 16:58:05 +0100

    Update Agent.java

--- src/Agent.java
+++ src/Agent.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
+
 public class Agent extends Utilisateur {
     private int id_agent;
+
+//associations
+    private ArrayList <Contrat> contrat;
+    private ArrayList <Facture> facture;
+    private ArrayList <Vehicule> vehicule;
     
+//constructeur
     public Agent (int id_agent) {
         this.id_agent=id_agent;
     }
@@ -10,14 +19,20 @@ public class Agent extends Utilisateur {
         
     }
 
-    public int getIdClient (){
+//get
+    public int getidAgent (){
         return this.id_agent;
     }
 
-    public void setIdClient (int id_cient){
+//set
+    public void setidAgent (int id_cient){
         this.id_agent = id_agent;
     }
 
+//toString
+
+
+//afficher liste d'agent
      public String afficher_agent(){
         return toString();
 

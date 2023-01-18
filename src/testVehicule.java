b36fe2e05c5ae02734a7d1ddcc5acfbf081e0d31
maissa70 <guezzimaissa@gmail.com>
2023-01-18 15:45:10 +0000

    MAJ

--- src/testVehicule.java
+++ src/testVehicule.java
@@ -14,16 +14,10 @@ vehicule[] tableauvehicules = new vehicule[nombre];
 for ( int i=1; i<tableauvehicules.length+1;i++) {
 System.out.println("Entrer l'id de la véhicule"+ " " + i);
 int idVehicule = sc.nextInt();
-System.out.println("Entrer le kilometrage");
-int kilometrage = sc.nextInt();
 System.out.println("Entrer le nombre de places");
 int nbrPlaces = sc.nextInt();
-System.out.println("Entrer prix de location");
-float prixlocation = sc.nextFloat();
 System.out.println("Entrer la marque");
 String marque = sc.next();
-System.out.println("Entrer l'immatriculation");
-String immatriculation = sc.next();
 System.out.println("Entrer la couleur");
 String couleur = sc.next();
 System.out.println("Entrer 1 si elle est dispo et 0 sinon");

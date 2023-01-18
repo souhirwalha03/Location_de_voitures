55008713da375c50b8e94790452444f11e161042
maissa70 <guezzimaissa@gmail.com>
2023-01-18 15:40:49 +0000

    mise à jour

--- src/vehicule.java
+++ src/vehicule.java
@@ -1,24 +1,20 @@
 public class vehicule {
 	
         private int idVehicule;
-        private int kilometrage ;
         private int nbrPlaces ;
         private float prixlocation;
         private String marque;
-        private String immatriculation ;
         private String couleur;
         private int dispo;  
     
        //constructeur
        
-        public vehicule(int idVehicule, int kilometrage , int nbrPlaces , float prixlocation , String marque , String immatriculation , String couleur, int dispo)
+        public vehicule(int idVehicule, int nbrPlaces , float prixlocation , String marque  , String couleur, int dispo)
         {
             this.idVehicule=idVehicule;
-            this.kilometrage=kilometrage;
             this.nbrPlaces=nbrPlaces;
             this.prixlocation=prixlocation;
             this.marque=marque;
-            this.immatriculation=immatriculation;
             this.couleur=couleur;
             this.dispo=dispo;        }
         public vehicule(){}
@@ -32,13 +28,6 @@ public class vehicule {
         public void setIdVehicule( int idVehicule ){
          this.idVehicule=idVehicule ;
         }
-    
-       public int getKilometrage(){
-        return this.kilometrage;
-       }
-       public void setKilometrage ( int kilometrage){
-          this.kilometrage=kilometrage;
-       }
     
        public int getNbrPlaces()
        { 
@@ -61,14 +50,6 @@ public class vehicule {
         public void setMarque( String marque ) {
             this.marque=marque ;
         }
-    
-        public String getImmatriculation(){
-            return this.immatriculation;
-        }
-        public void setImmatriculation( String immatriculation){
-            this.immatriculation=immatriculation ;
-        }
-    
         public String getCouleur(){
             return this.couleur;
         }
@@ -77,6 +58,7 @@ public class vehicule {
         }
         public int getDispo(){
             return this.dispo;}
+            
         public void setDispo(int dispo){
         this.dispo=dispo;
         }
@@ -84,7 +66,7 @@ public class vehicule {
         ///methode d'affichage 
         public void affichervehicule(vehicule V){
          
-        System.out.println("Id:"+ idVehicule + "Kilometrage\n"+ kilometrage + "Nombre de places:\n"+nbrPlaces + "Prix de location\n"+prixlocation + "Marque\n"+ marque + "Immatriculation:\n"+ immatriculation +"Disponibilité:\n" + dispo);
+        System.out.println("Id:"+ idVehicule + "Nombre de places:\n"+nbrPlaces + "Prix de location\n"+prixlocation + "Marque\n"+ marque  +"Disponibilité:\n" + dispo);
         }
 
         /// methode de verification de disponibilité 

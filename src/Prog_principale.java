8d652eb74555ab6983453852716383ed71f30d50
maissa70 <guezzimaissa@gmail.com>
2023-01-19 14:33:51 +0000

    mise à jour

--- src/Prog_principale.java
+++ src/Prog_principale.java
@@ -30,7 +30,7 @@ public class Prog_principale {
         //Vehicules
         Scanner sc =new Scanner(System.in);
         Vehicule vehicule1 = new Vehicule(85889, 1000, "Polo" , 0, Ag3);
-        Vehicule vehicule2 = new Vehicule(85899, 1100, "Fiat" , 1, Ag3);
+        Vehicule vehicule2 = new Vehicule(85899, 1100, "Fiat" , 0, Ag3);
         Vehicule vehicule3 = new Vehicule(85000, 1500, "Mercedes" , 1, Ag3);
          //ArrayList<Vehicule> vehicules = new ArrayList<Vehicule>();
         Vehicule.vehicules.add(vehicule1);
@@ -38,12 +38,20 @@ public class Prog_principale {
         Vehicule.vehicules.add(vehicule3);
         System.out.println("La liste des vehicules:");
         Vehicule.afficher_vehicules(Vehicule.vehicules);
+
         // disponibilité
       System.out.println(Vehicule.EstDisponible(vehicule1));
       System.out.println(Vehicule.EstDisponible(vehicule2));
       System.out.println(Vehicule.EstDisponible(vehicule3));
       
-    
+      //Locations
+     
+      Location location1=new Location(client1, vehicule1, 5, 170);
+      Location location2=new Location(client1, vehicule2, 15, 180);
+      System.out.println("Le montant à payer est: ");
+      System.out.println(Facture.Calcul_montant(location1));
+      System.out.println(Facture.Calcul_montant(location2));
+      
 		
     }
 }

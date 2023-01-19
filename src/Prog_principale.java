b367e01577a73519af4fe72d5fb0d389b67ac321
maissa70 <guezzimaissa@gmail.com>
2023-01-19 17:05:42 +0000

    NEW

--- src/Prog_principale.java
+++ src/Prog_principale.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Prog_principale {
     public static void main(String[] args) {
-        //Clients
+    //Clients
         Client client1 = new Client(123456,"emna", "mmmm");
         Client client2 = new Client(253859,"leila", "pppp");
         Client client3 = new Client(253564,"adem", "DDDDD");
@@ -15,7 +15,7 @@ public class Prog_principale {
         Client.affClients(clients);
         
 
-        //Agents
+    //Agents
         Agent Ag1 = new Agent(124658,"aaa", "bbb");
         Agent Ag2 = new Agent(365685,"ccc", "dd");
         Agent Ag3 = new Agent(561225,"ee", "ff");
@@ -27,7 +27,7 @@ public class Prog_principale {
         Agent.affAgents(Agents);
     
 
-        //Vehicules
+    //Vehicules
         Scanner sc =new Scanner(System.in);
         Vehicule vehicule1 = new Vehicule(85889, 1000, "Polo" , 0, Ag3);
         Vehicule vehicule2 = new Vehicule(85899, 1100, "Fiat" , 0, Ag3);
@@ -39,19 +39,21 @@ public class Prog_principale {
         System.out.println("La liste des vehicules:");
         Vehicule.afficher_vehicules(Vehicule.vehicules);
 
-        // disponibilité
-      System.out.println(Vehicule.EstDisponible(vehicule1));
-      System.out.println(Vehicule.EstDisponible(vehicule2));
-      System.out.println(Vehicule.EstDisponible(vehicule3));
+    // disponibilité
+        System.out.println(Vehicule.EstDisponible(vehicule1));
+        System.out.println(Vehicule.EstDisponible(vehicule2));
+        System.out.println(Vehicule.EstDisponible(vehicule3));
       
-      //Locations
+    //Locations
      
-      Location location1=new Location(client1, vehicule1, 5, 170);
-      Location location2=new Location(client1, vehicule2, 15, 180);
-      System.out.println("Le montant à payer est: ");
-      System.out.println(Facture.Calcul_montant(location1));
-      System.out.println(Facture.Calcul_montant(location2));
-      
-		
+         Location location1=new Location(client1, vehicule1, 5, 170);
+         Location location2=new Location(client1, vehicule2, 15, 180);
+         System.out.println("Le montant à payer est: ");
+         System.out.println(Facture.Calcul_montant(location1));
+         System.out.println(Facture.Calcul_montant(location2));
+       
+    //consulter vehicule
+	 System .out.println(vehicule1.toString());
+
     }
 }
